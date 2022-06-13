5991936471c4367ca46ba12083a6eca8844b75bd
Jeff Bridges <jbridges1119@gmail.com>
2022-06-13 21:24:48 +0000

    added npm morgan and body-parser and then created ejs file for new url inputs. Updated express_server with post request.

--- express_server.js
+++ express_server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
-app.set("view engine", "ejs")
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -25,6 +29,10 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
@@ -35,3 +43,7 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
