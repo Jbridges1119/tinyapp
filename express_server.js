7c1b0169b6c2f280b0cf01ff69783294ed402449
Jeff Bridges <jbridges1119@gmail.com>
2022-06-14 16:33:45 +0000

    Added edit and delete buttons and fucntions.

--- express_server.js
+++ express_server.js
@@ -4,58 +4,86 @@ const PORT = 8080; // default port 8080
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 
+//MIDDLEWARE BEING RUN
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+//URL DATABASE
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+//SEND TEXT SAYING HELLO
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+//REQUEST FOR URLS IN JSON FORMAT
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+//SEND TEXT SAYING HELLO WORLD
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+//SERVER RESPONSE WHEN STARTED
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+//REQUEST CREATE TINYURL PAGE
 app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 });
 
+//REQUEST MAIN PAGE
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
-
+//Create new url page
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
+//SUBMITTING NEW URL
 app.post("/urls", (req, res) => {
-    // Log the POST request body to the console
-
-  urlDatabase[generateRandomString()] = req.body.longURL
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const key = generateRandomString();
+  urlDatabase[key] = req.body.longURL;
+  res.redirect(`/urls/${key}`);
 });
 
+//REQUEST SPECIFIC SHORT URL INFO / EDIT PAGE
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
 
+//SUBMIT EDITED ULR
+app.post('/urls/:shortURL', (req, res) => {
+  console.log(req.params)
+  urlDatabase[req.params.shortURL] = req.body.longURL
+  res.redirect(`/urls`)
+})
+
+//DELETE URL
+app.post("/urls/:shortURL/delete", (req,res) => {
+   delete urlDatabase[req.params.shortURL]
+  res.redirect(`/urls`)
+})
+
+
+//GENERATE RANDOM SHORT URL KEY
 function generateRandomString() {
-    const character = '0123456789AaBbCcDdEeFfGgHhIiJjKkLl0123456789MmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789';
-    let key = "";
-    while (key.length < 6){
+  const character = 'AaBbCcDdEeFfGgHhIiJjKkLl0123456789MmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789';
+  let key = "";
+  while (key.length < 6) {
     key += character.charAt(Math.floor(Math.random() * 62));
-    }
-    return key;
-};
+  }
+  return key;
+}
