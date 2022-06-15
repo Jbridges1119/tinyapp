ad69b47357a7a2d0fcbc138d7576c784d524836e
Jeff Bridges <jbridges1119@gmail.com>
2022-06-15 16:14:42 +0000

    replaced login nav bar with login page. Refactord code for new way to login.

--- express_server.js
+++ express_server.js
@@ -23,18 +23,45 @@ const urlDatabase = {
 };
 
 //USER DATA STORAGE
-const users = { 
+const users = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "u@a.com", 
+    id: "userRandomID",
+    email: "u@a.com",
     password: "purple"
   },
- "user2RandomID": {
-    id: "user2RandomID", 
-    email: "u2@a.com", 
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "u2@a.com",
     password: "funk"
   }
-}
+};
+
+//GENERATE RANDOM SHORT URL KEY
+const generateRandomString = () => {
+  const character = 'AaBbCcDdEeFfGgHhIiJjKkLl0123456789MmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789';
+  let key = "";
+  while (key.length < 6) {
+    key += character.charAt(Math.floor(Math.random() * 62));
+  }
+  return key;
+};
+
+//EMAIL AVAILABILITY FUNCTION
+const emailNotPresent = (newEmail, users) => {
+  for (let key in users) {
+    if (newEmail === users[key]["email"]) {
+      return false;
+    }
+  } return true;
+};
+
+const locateID = (emailSubmitted, users) => {
+  for (let key in users) {
+    if (emailSubmitted === users[key]["email"]) {
+      return key;
+    }
+  }
+};
 
 //SEND TEXT SAYING HELLO
 app.get("/", (req, res) => {
@@ -59,15 +86,23 @@ app.listen(PORT, () => {
 //REQUEST REGISTRATION PAGE
 app.get(`/register`, (req, res) => {
   const templateVars = {
-    username: req.cookies["username"],
+    username: users[req.cookies["user_id"]]
   };
-  res.render(`register`, templateVars)
-})
+  res.render(`register`, templateVars);
+});
+
+//REQUEST LOGIN PAGE
+app.get('/login', (req, res) => {
+  const templateVars = {
+    username: users[req.cookies["user_id"]]
+  };
+  res.render(`login`, templateVars);
+});
 
 //REQUEST CREATE TINYURL PAGE
 app.get('/urls/new', (req, res) => {
   const templateVars = {
-    username: req.cookies["username"],
+    username: users[req.cookies["user_id"]]
   };
   res.render('urls_new', templateVars);
 });
@@ -76,7 +111,7 @@ app.get('/urls/new', (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = {
     urls: urlDatabase,
-    username: req.cookies["username"]
+    username: users[req.cookies["user_id"]]
   };
   res.render("urls_index", templateVars);
 });
@@ -86,7 +121,7 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
-    username: req.cookies["username"]
+    username: users[req.cookies["user_id"]]
   };
   res.render("urls_show", templateVars);
 });
@@ -100,30 +135,63 @@ app.post("/urls", (req, res) => {
 
 //REQUEST SPECIFIC SHORT URL INFO / EDIT PAGE
 app.get("/u/:shortURL", (req, res) => {
-  const templateVars = {
-    username: req.cookies["username"],
-  };
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
+
+//SUBMIT REGISTER
+app.post('/register', (req, res) => {
+  const id = generateRandomString();
+  const {email, password} = req.body;
+  //If email or password are blank - return an error
+  if (email === "" || password === "") {
+    return res.status(400).send('Empty Email or Password');
+  }
+  //If email is taken - return an error
+  if (!emailNotPresent(email, users)) {
+    return res.status(400).send('Email already taken');
+  }
+  users[id] = {
+    "id": id,
+    "email": email,
+    "password": password
+  };
+  res.cookie("user_id", id);
+  res.redirect('/urls');
+});
+
+
 //SUBMIT LOGIN
 app.post('/login', (req, res) => {
-  res.cookie("username", req.body.username);
+  const {email, password} = req.body;
+  const user = locateID(email, users);
+  //If email isn't in database - return an error
+  if (emailNotPresent(email, users)) {
+    return res.status(403).send('Incorrect Email');
+  }
+  //If email is located but password does not match - return error
+  if (password !== users[user]["password"]) {
+    return res.status(403).send('Incorrect Password');
+  }
+  res.cookie("user_id", user);
   res.redirect(`/urls`);
 });
 
+
 //SUBMIT LOGOUT
 app.post('/logout', (req, res) => {
-  res.clearCookie('username');
+  res.clearCookie('user_id');
   res.redirect(`/urls`);
 });
+
 
 //SUBMIT EDITED ULR
 app.post('/urls/:shortURL', (req, res) => {
   urlDatabase[req.params.shortURL] = req.body.longURL;
   res.redirect(`/urls`);
 });
+
 
 //DELETE URL
 app.post("/urls/:shortURL/delete", (req,res) => {
@@ -132,12 +200,3 @@ app.post("/urls/:shortURL/delete", (req,res) => {
 });
 
 
-//GENERATE RANDOM SHORT URL KEY
-const generateRandomString = () => {
-  const character = 'AaBbCcDdEeFfGgHhIiJjKkLl0123456789MmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789';
-  let key = "";
-  while (key.length < 6) {
-    key += character.charAt(Math.floor(Math.random() * 62));
-  }
-  return key;
-};
